c26fc64082c15331f36c35efcad2e1226a66a260
Sergei Trofimovich <slyich@gmail.com>
2022-02-05 09:33:40 +0000

    overlays/sequential.nix: drop upstreamed fixes

--- overlays/sequential.nix
+++ overlays/sequential.nix
@@ -13,26 +13,8 @@ final: prev: gen_overrides prev [
   # TODO: fix these upstream
   # TODO: pull some failures from PR and local hacks
 
-  #alliance-unstable> vmcasmld.c:26:10: fatal error: drucompi_y.h: No such file or directory
-  "alliance"
-
-  "ats2" # make[2]: *** No rule to make target 'patscc.dats', needed by 'patscc_dats.c'.  Stop.
-
-  # early failure, possibly missing imake dep
-  # bash: line 1: ./config/imake/imake: No such file or directory
-  # Failed to build upstream version.
-  "cdesktopenv"
-
-  "deadpixi-sam" # mysterious build failures
-
   "gnat6" # fails sometimes
-
   "heimdal" # elusive failure
-
-  #make[3]: *** No rule to make target '../util/libfaxutil.so.7.0.4', needed by 'faxmsg'.  Stop.
-  "hylafaxplus" # parallel make
-
-  "lazarus-qt"
 
   #app-indicator.c:45:10: fatal error: application-service-marshal.h: No such file or directory
   #  45 | #include "application-service-marshal.h"
@@ -41,40 +23,6 @@ final: prev: gen_overrides prev [
   # "libappindicator-gtk3" # headers deps
   # "libappindicator-gtk2" # headers deps
 
-  "mlkit"
-  "mlton20130715"
-  "mltonHEAD"
-
-  "munin" # lacks parallel support in makefile, fails instantly
-
-  "netboot" # libtool uses incomplete files?
-
-  "patchutils" # parallel tests fail, file collision
-
-  #"ocaml-mysql" # parallel build failure
-
-  #"camlzip"
-  #"scsh" # file access, serial?
-  #"satallax"
-
-  "tomboy" # config
-
   # uae> make[2]: *** No rule to make target 'build68kc'.  Stop.
   "uae"
-
-  #dispatch_key.c:25:10: fatal error: compute_crc32.h: No such file or directory
-  #  25 | #include "compute_crc32.h"
-  #"perl5.34.0-Cache-Memcached-Fast"
-
-  #> Can't write-open blib/man5/razor-agent.conf.5: No such file or directory
-  #"perl5.34.0-Razor2-Client-Agent"
-
-  #ocaml-ng.ocamlPackages_4_12.stdcompat
-  #ocamlPackages.camlidl
-  #ocamlPackages_4_03.camlp4
-
-  #postgresqlPackages.repmgr
-
-  # sent upstream:
-  #"ipvsadm"
 ]
