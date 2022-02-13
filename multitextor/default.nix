fd301c734a74f58218d4a7f36b461dbb7a7549c7
Sergei Trofimovich <slyich@gmail.com>
2022-02-13 21:02:59 +0000

    multitextor: 2022-02-04 -> 2022-02-09
    Upstreamed patch.

--- multitextor/default.nix
+++ multitextor/default.nix
@@ -1,7 +1,6 @@
 { lib
 , stdenv
 , fetchFromGitHub
-, fetchpatch
 , cmake
 , gpm
 , ncurses
@@ -11,23 +10,14 @@
 
 stdenv.mkDerivation rec {
   pname = "multitextor";
-  version = "unstable-2022-02-04";
+  version = "unstable-2022-02-09";
 
   src = fetchFromGitHub {
     owner = "vikonix";
     repo = "multitextor";
-    rev = "90b6877cc08bb7c6a660fe361b641b3941c84cbd";
-    sha256 = "sha256-rlB48b7ebLbpG/lLALwtMw50Mlfn7CocMZG8i9QnA0c=";
+    rev = "e8cc2b70afd934370e5dad306fb78809ac6f9651";
+    sha256 = "sha256-4qcp5YqFIqS2FNssP28izfZ1QGQWw1ABaRQAW3NFxIo=";
   };
-
-  patches = [
-    # https://github.com/vikonix/multitextor/pull/21
-    (fetchpatch {
-      name = "obey-prefix.patch";
-      url = "https://github.com/vikonix/multitextor/commit/09c390173fc4e1e15d0073652120edc59fe1051b.patch";
-      sha256 = "sha256-Qr6AAGCaKWAmkqeB2w6N1HHga5FhEB3ZhdM4UBi7IdE=";
-    })
-  ];
 
   nativeBuildInputs = [ cmake ];
   buildInputs = [
@@ -40,7 +30,7 @@ stdenv.mkDerivation rec {
   # Update as:
   #    nix-shell ./maintainers/scripts/update.nix --argstr package multitextor --arg include-overlays true
   passthru.updateScript = unstableGitUpdater {
-    url = "sha256-rlB48b7ebLbpG/lLALwtMw50Mlfn7CocMZG8i9QnA0c=";
+    url = "https://github.com/vikonix/multitextor";
   };
 
   meta = with lib; {
