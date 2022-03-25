e551788fbd5c1f492defac63c66e5d1935341692
Sergei Trofimovich <slyich@gmail.com>
2022-03-25 07:59:41 +0000

    vcmi: unstable-2022-01-30 -> unstable-2022-03-21

--- vcmi/default.nix
+++ vcmi/default.nix
@@ -21,13 +21,13 @@
 
 mkDerivation rec {
   pname = "vcmi";
-  version = "unstable-2022-01-30";
+  version = "unstable-2022-03-21";
 
   src = fetchFromGitHub {
     owner = "vcmi";
     repo = "vcmi";
-    rev = "ea2931c6ea91f9bb44fdd9f0b753c3c862084a29";
-    sha256 = "sha256-ZUWYVrhojpj05dAqJI7WBUc5Hoj8N51KgbMp/rA9qKU=";
+    rev = "73906d5a9754ee37593e317befb14092a550e67c";
+    sha256 = "sha256-PGIVLBllsVikozVmtuM6R6pfapL+vX5QAmam0DNxU70=";
     fetchSubmodules = true;
   };
 
