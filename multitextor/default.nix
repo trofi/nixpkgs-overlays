82f9e51b635f0c7bd52084cab8f6906dd0d367d1
Sergei Trofimovich <slyich@gmail.com>
2022-04-02 09:19:42 +0100

    multitextor: unstable-2022-03-09 -> unstable-2022-03-27

--- multitextor/default.nix
+++ multitextor/default.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation rec {
   pname = "multitextor";
-  version = "unstable-2022-03-09";
+  version = "unstable-2022-03-27";
 
   src = fetchFromGitHub {
     owner = "vikonix";
     repo = "multitextor";
-    rev = "8b2df7f138293af6a59b7fe0dae854969aacd0c9";
-    sha256 = "sha256-eEsZAHR6J6YPQxgcvfX4VmEAeBodNNMlZf/GoOtHBlY=";
+    rev = "658c065f391eb82a4d2181787732a04492cac9eb";
+    sha256 = "sha256-4oqScZeQgGbkPvrR/yS6vtYVrr+RiJGa24y+xtJyJUg=";
   };
 
   nativeBuildInputs = [ cmake ];
