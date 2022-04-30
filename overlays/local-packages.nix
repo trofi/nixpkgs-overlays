135554b5fe3e3099503906f973d4c7d5c1910082
Sergei Trofimovich <slyich@gmail.com>
2022-04-30 07:42:37 +0100

    drop unused checkpatch and chessutil

--- overlays/local-packages.nix
+++ overlays/local-packages.nix
@@ -14,10 +14,6 @@ final: prev: {
 
   corsix-th = final.callPackage ../corsix-th {};
 
-  checkpatch = final.callPackage ../checkpatch {};
-
-  chessutils = final.callPackage ../chessutils {};
-
   iowatcher = final.callPackage ../iowatcher {};
 
   multitextor = final.callPackage ../multitextor {};
