4b14e5cf1010448d3aa064922b2b78cc0f1c8631
Sergei Trofimovich <slyich@gmail.com>
2022-05-26 21:41:06 +0100

    Revert "nix: add gcc-13 patch for <stdint.h>"
    This reverts commit 9457efa9511eb3b337c8f44f4addcaa9745f0118.

--- overlays/local-patches.nix
+++ overlays/local-patches.nix
@@ -26,7 +26,6 @@ final: prev: {
   nixUnstable = prev.nixUnstable.overrideAttrs(oa: {
     patches = (oa.patches or []) ++ [
       ../nix/0001-src-libstore-derivations.cc-avoid-istringstream-on-h.patch
-      ../nix/0001-src-libutil-json.cc-add-missing-cstdint-include-for-.patch
     ];
     dontStrip = true;
     separateDebugInfo = false;
