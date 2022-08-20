8978ecff2486d6ae6eff039ff3b7bd5b8787b923
Sergei Trofimovich <slyich@gmail.com>
2022-08-20 08:55:30 +0100

    ski: add a shell

---
+++ shells/ski-git/shell.nix
@@ -0,0 +1,34 @@
+{ pkgs ? import ~/nm {} }:
+  pkgs.mkShell {
+    nativeBuildInputs = with pkgs; [
+      # build deps
+      elfutils
+      libbfd
+      libiberty
+      ncurses
+      motif
+      ## x11
+      xorg.libX11
+      xorg.libXt
+      xorg.libXext
+      ## gtk
+      gnome2.libglade
+      gnome2.libgnomeui
+
+      # maintainer tools
+      autoconf
+      automake
+      bison
+      flex
+      gperf
+      libtool
+      pkg-config
+
+      # for release tarballs
+      pkgs.lzip
+      pkgs.xz
+    ];
+    shellHook = ''
+      mkj() { make -j$(nproc); }
+    '';
+  }
