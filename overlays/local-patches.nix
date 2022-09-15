91a5fe94906e1a57a1b86f13a02e13f3ed34ad7a
Sergei Trofimovich <slyich@gmail.com>
2022-09-15 22:34:19 +0100

    drop local nix and binutils patches

--- overlays/local-patches.nix
+++ overlays/local-patches.nix
@@ -1,9 +1,4 @@
 final: prev: {
-  # https://github.com/NixOS/nixpkgs/pull/169907
-  binutils-unwrapped-all-targets = prev.binutils-unwrapped-all-targets.overrideAttrs(oa: {
-    postFixup = "";
-  });
-
   claws-mail = prev.claws-mail.overrideAttrs(oa: {
     patches = (oa.patches or []) ++ [
       ../claws-mail/show-all-text.patch
@@ -14,21 +9,5 @@ final: prev: {
     patches = (oa.patches or []) ++ [
       ../cvise/reverse-lines.patch
     ];
-  });
-
-  liferea = prev.liferea.overrideAttrs(oa: {
-    src = let ver = "1.13.8"; in prev.fetchurl {
-      url = "https://github.com/lwindolf/liferea/releases/download/v${ver}/liferea-${ver}.tar.bz2";
-      sha256 = "sha256-EAiugFGSgWsZDwfGyOCUJWvFuIBi9drxpxAlC+0pSHQ=";
-    };
-  });
-
-  nixUnstable = prev.nixUnstable.overrideAttrs(oa: {
-    patches = (oa.patches or []) ++ [
-      ../nix/0001-src-libstore-derivations.cc-avoid-istringstream-on-h.patch
-    ];
-    dontStrip = true;
-    separateDebugInfo = false;
-    NIX_CFLAGS_COMPILE = "-ggdb3";
   });
 }
