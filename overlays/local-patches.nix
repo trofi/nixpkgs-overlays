8d1240f2fbabb632348215ae0ad6021087b66925
Sergei Trofimovich <slyich@gmail.com>
2022-10-02 11:27:27 +0100

    overlays/local-patches.nix: only patch xwayland with libXfonts2 symlink hack

--- overlays/local-patches.nix
+++ overlays/local-patches.nix
@@ -11,11 +11,20 @@ final: prev: {
     ];
   });
 
+  /*
   xorg = prev.xorg.overrideScope' (f: p: {
     libXfont2 = p.libXfont2.overrideAttrs (oa: {
       patches = (oa.patches or []) ++ [
         ../libXfont2/nofollow.patch
       ];
     });
-  });
+  }) // { inherit (prev) xlibsWrapper; };
+  */
+  xwayland = prev.xwayland.override {
+    libXfont2 = prev.xorg.libXfont2.overrideAttrs (oa: {
+      patches = (oa.patches or []) ++ [
+        ../libXfont2/nofollow.patch
+      ];
+    });
+  };
 }
