eccaea2bb83c164af20aafb30f14337da29b5809
Sergei Trofimovich <slyich@gmail.com>
2022-11-17 16:22:46 +0000

    overlays: backport missing output

--- overlays/local-patches.nix
+++ overlays/local-patches.nix
@@ -11,6 +11,20 @@ final: prev: {
     ];
   });
 
+  nixUnstable = prev.nixUnstable.overrideAttrs(oa: {
+    patches = (oa.patches or []) ++ [
+      # https://github.com/NixOS/nix/pull/7283
+      (prev.fetchpatch {
+        name = "dupes.patch";
+        url = "https://github.com/NixOS/nix/commit/7e162c69fe6cbfb929b5356a7df9de5c25c22565.patch";
+        hash = "sha256-8DX4spZOvgg5214HZdaUrFv4jkYEQhjckxOSiAkC0Cg=";
+      })
+    ];
+    #dontStrip = true;
+    #separateDebugInfo = false;
+    #NIX_CFLAGS_COMPILE = "-ggdb3";
+  });
+
   /*
   xorg = prev.xorg.overrideScope' (f: p: {
     libXfont2 = p.libXfont2.overrideAttrs (oa: {
