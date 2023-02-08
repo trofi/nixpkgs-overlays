bdeea008f95d343b138b28853139a450cd6a8a9b
Sergei Trofimovich <slyich@gmail.com>
2023-02-08 08:36:14 +0000

    vifm: apply pending `:help` fix

--- overlays/local-patches.nix
+++ overlays/local-patches.nix
@@ -11,15 +11,15 @@ final: prev: {
     ];
   });
 
-  /*
-  xorg = prev.xorg.overrideScope' (f: p: {
-    libXfont2 = p.libXfont2.overrideAttrs (oa: {
-      patches = (oa.patches or []) ++ [
-        ../libXfont2/nofollow.patch
-      ];
-    });
-  }) // { inherit (prev) xlibsWrapper; };
-  */
+  # a milder form of https://github.com/NixOS/nixpkgs/pull/215272
+  vifm = prev.vifm.overrideAttrs (oa: {
+    nativeBuildInputs = [ prev.perl ] ++ oa.nativeBuildInputs;
+    postPatch = ''
+      # Avoid '#!/usr/bin/env perl' reverences to build help.
+      patchShebangs --build src/helpztags
+    '' + (oa.postPatch or "");
+  });
+
   xwayland = prev.xwayland.override {
     libXfont2 = prev.xorg.libXfont2.overrideAttrs (oa: {
       patches = (oa.patches or []) ++ [
