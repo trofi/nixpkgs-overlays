343a3a1c1648bddcd82a44c98d7f041d290e7490
Sergei Trofimovich <slyich@gmail.com>
2023-02-10 07:40:35 +0000

    vifm: drop upstreamed override

--- overlays/local-patches.nix
+++ overlays/local-patches.nix
@@ -11,15 +11,6 @@ final: prev: {
     ];
   });
 
-  # a milder form of https://github.com/NixOS/nixpkgs/pull/215272
-  vifm = prev.vifm.overrideAttrs (oa: {
-    nativeBuildInputs = [ prev.perl ] ++ oa.nativeBuildInputs;
-    postPatch = ''
-      # Avoid '#!/usr/bin/env perl' reverences to build help.
-      patchShebangs --build src/helpztags
-    '' + (oa.postPatch or "");
-  });
-
   xwayland = prev.xwayland.override {
     libXfont2 = prev.xorg.libXfont2.overrideAttrs (oa: {
       patches = (oa.patches or []) ++ [
