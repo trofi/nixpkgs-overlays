6e31fc96b1eb11759d1e3526d88856598d98869c
Sergei Trofimovich <slyich@gmail.com>
2023-07-02 22:21:03 +0100

    shells/gcc-git/shell-fhs.nix: new shell for FHS builds
    non-FHS shells can't build a gcc readily available to build final
    binaries due to non-standard nixpkgs wrapper. FHS shell should turn it
    into something that Just Works: /usr/bin and /usr/include are there
    and are used by just built `gcc`.

---
+++ shells/gcc-git/shell-fhs.nix
@@ -0,0 +1,34 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let e =
+  pkgs.buildFHSEnv {
+    name = "gcc-git-build-env";
+    targetPkgs = ps: with ps; [
+      # library depends
+      gmp gmp.dev
+      isl
+      libffi libffi.dev
+      libmpc
+      mpfr mpfr.dev
+      zlib zlib.dev
+
+      # git checkout need flex as they are not complete release tarballs
+      m4
+      bison
+      flex
+      texinfo
+
+      # test harness
+      dejagnu
+      autogen
+
+      # valgrind annotations
+      valgrind
+
+      # toolchain itself
+      gcc
+      stdenv.cc
+      stdenv.cc.libc stdenv.cc.libc_dev
+    ];
+  };
+in e.env
