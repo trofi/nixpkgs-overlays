99a3fca4d39eabc91661d9247bd23fe44c48f6e1
Sergei Trofimovich <slyich@gmail.com>
2023-11-15 20:25:09 +0000

    cvise: fix --command= shebang

--- overlays/local-patches.nix
+++ overlays/local-patches.nix
@@ -8,6 +8,17 @@ final: prev: {
   cvise = prev.cvise.overrideAttrs(oa: {
     patches = (oa.patches or []) ++ [
       ../cvise/reverse-lines.patch
+
+      # Keep locally until it's upstream:
+      #   https://github.com/NixOS/nixpkgs/pull/267736
+      #
+      # Refer to shell via /usr/bin/env:
+      #   https://github.com/marxin/cvise/pull/121
+      (prev.fetchpatch {
+        name = "env-shell.patch";
+        url = "https://github.com/marxin/cvise/commit/6a416eb590be978a2ad25c610974fdde84e88651.patch";
+        hash = "sha256-Kn6+TXP+wJpMs6jrgsa9OwjXf6vmIgGzny8jg3dfKWA=";
+      })
     ];
   });
 
