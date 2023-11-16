cb0a162a8ac43c15aa4aebcce5eb57bbfdd02e14
Sergei Trofimovich <slyich@gmail.com>
2023-11-16 19:08:08 +0000

    libglvnd: revert "apply a heavyweight patch for 64-bit inode support"
    This reverts commit c51f38dc9a901bf22b7df1dfc2fa7cd80fd5019d.
    
    Can't use that alone without fixing a few more 64-bit uncleanliness that
    drags in even more rebuilds. Let's wait for `staging` propagation.

--- overlays/local-patches.nix
+++ overlays/local-patches.nix
@@ -22,21 +22,6 @@ final: prev: {
     ];
   });
 
-  libglvnd = prev.libglvnd.overrideAttrs(oa: {
-    patches = (oa.patches or []) ++ [
-      # Keep locally until it's in `master` of `nixpkgs`:
-      #   https://nixpk.gs/pr-tracker.html?pr=267505
-      #
-      # Enable 64-bit file APIs on 32-bit systems:
-      #   https://gitlab.freedesktop.org/glvnd/libglvnd/-/merge_requests/288
-      (prev.fetchpatch {
-        name = "large-file.patch";
-        url = "https://gitlab.freedesktop.org/glvnd/libglvnd/-/commit/956d2d3f531841cabfeddd940be4c48b00c226b4.patch";
-        hash = "sha256-Y6YCzd/jZ1VZP9bFlHkHjzSwShXeA7iJWdyfxpgT2l0=";
-      })
-    ];
-  });
-
   xwayland = prev.xwayland.override {
     libXfont2 = prev.xorg.libXfont2.overrideAttrs (oa: {
       patches = (oa.patches or []) ++ [
