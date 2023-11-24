2f5effa513106c45cefde7e13510a2429b6f2954
Sergei Trofimovich <slyich@gmail.com>
2023-11-24 10:19:20 +0000

    duperemove: backport SIGSEGV on empty files

--- overlays/local-patches.nix
+++ overlays/local-patches.nix
@@ -20,6 +20,13 @@ final: prev: {
         url = "https://github.com/markfasheh/duperemove/commit/eeeddca9f8820594d872366f74a4c01fc9377d9c.patch";
         hash = "sha256-Q6kwNJEJIXSAI+Ec968KH0uloohAwq1Tu72pzyrQJVU=";
       })
+      # Fix for SIGSEGV:
+      #   https://github.com/markfasheh/duperemove/issues/332
+      (prev.fetchpatch {
+        name = "SEGV-on-extents.patch";
+        url  = "https://github.com/markfasheh/duperemove/commit/9912c03c16af67e33b5dc36052e8faed9a17749d.patch";
+        hash = "sha256-+YGAtfILWpcmYtt5hmKut8ND3+yJLZ76iInIYqyESOQ=";
+      })
     ];
   });
 
