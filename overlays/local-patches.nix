3fbb12260aae9b07dd82ed81b1ceef871ff03848
Sergei Trofimovich <slyich@gmail.com>
2024-08-24 14:24:49 +0100

    diffoscope: apply pending patch for directory diffing

--- overlays/local-patches.nix
+++ overlays/local-patches.nix
@@ -11,6 +11,18 @@ final: prev: {
     ];
   });
 
+  diffoscope = prev.diffoscope.overrideAttrs(oa: {
+    patches = (oa.patches or []) ++ [
+      # Fix diff on symlinks to directories:
+      #   https://salsa.debian.org/reproducible-builds/diffoscope/-/merge_requests/144
+      (prev.fetchpatch {
+        name = "symlink-dir.patch";
+        url = "https://salsa.debian.org/reproducible-builds/diffoscope/-/merge_requests/144.patch";
+        hash = "sha256-juLPO67mYFg2vXPF5piR7W2YY2HOuOVAiGpmYwpjTkQ=";
+      })
+    ];
+  });
+
   nixVersions = prev.nixVersions // {
     unstable = (prev.nixVersions.latest or prev.nixVersions.unstable).overrideAttrs(oa: {
       patches = (oa.patches or []) ++ [
