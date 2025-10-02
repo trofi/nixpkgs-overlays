d18212efc434ed1eef9d2b1b308edcd1e3f37100
Sergei Trofimovich <slyich@gmail.com>
2025-10-03 00:00:20 +0100

    shells: add a trivial weston-git shell

---
+++ shells/weston-git/shell.nix
@@ -0,0 +1,46 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    glslang
+    meson
+    ninja
+    pkg-config
+    python3
+    wayland-protocols
+    wayland-scanner
+
+    aml
+    cairo
+    fontconfig
+    freerdp
+    glib
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    lcms2
+    libdisplay-info
+    libdrm
+    libevdev
+    libgbm
+    libglvnd
+    libinput
+    libjpeg
+    libva
+    libwebp
+    libxcursor
+    libxkbcommon
+    lua5_4
+    neatvnc
+    pam
+    pango
+    pipewire
+    pixman
+    seatd
+    vulkan-loader
+    wayland
+    xcb-util-cursor
+    xorg.libxcb
+    xwayland
+  ];
+}
